d5de8f3dc763b55e0bb7952cb71a1821bbc4ded6
daveaschalew418-blip <daveaschalew418@gmail.com>
2026-07-07 16:39:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,14 @@
-const CACHE_NAME = 'offline-cal-v3';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'https://cdn-icons-png.flaticon.com/512/747/747310.png'
-];
+const CACHE_NAME = 'lifesync-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME ? caches.delete(k) : null))).then(() => self.clients.claim()));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
